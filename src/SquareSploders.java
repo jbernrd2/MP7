f1e98c041848ff22ad25f1ea07efe477bf051d37
Jack Bernard <jbernrd2@illinois.edu>
2017-12-10 16:27:12 -0600

    Added moving targets, deletes laser instance when it hits a target

--- src/SquareSploders.java
+++ src/SquareSploders.java
@@ -1,4 +1,5 @@
 import java.io.File;
+
 import java.awt.event.KeyEvent;
 import edu.illinois.cs.cs125.lib.zen.Zen;
 
@@ -9,8 +10,8 @@ public class SquareSploders {
         int x = 100, y = 900; int[] a = null; Target[] targetArray = new Target[5];
 
         boolean s1 = false; boolean s2 = true; Laser laser1 = null; Target target = null;
-        Laser laser2 = null; Laser laser3 = null; Laser[] laserray = new Laser[5];
-        int count = 0; Laser activeLaser = new Laser(); laser1 = activeLaser; int count1 = 0;
+        Laser laser2 = null; Laser laser3 = null; Laser[] laserray = new Laser[5]; int[] xt = {0,0,0,0,0}, yt = {0,0,0,0,0}; 
+        int count = 0; Laser activeLaser = new Laser(); laser1 = activeLaser; int count1 = 0; double random;
 
         ClassLoader classLoader = SpriteMoveFlipBuffer.class.getClassLoader();
         File spriteFile = new File(classLoader.getResource("sprite1.gif").getFile()); int xj = 225; int yj = 500;
@@ -20,11 +21,31 @@ public class SquareSploders {
             xj = xj + 40;
 
         }
+        targetArray[0] = new Target(225, 500, 20, 20);
+        targetArray[1] = new Target(250, 525, 20, 20);
+        targetArray[2] = new Target(200, 525, 20, 20);
+        targetArray[3] = new Target(215, 475, 20, 20);
+        targetArray[4] = new Target(235, 475, 20, 20);
 
         while (Zen.isRunning()) {
-            for (j = 0; j < targetArray.length; j++) {
-                if (targetArray[j] != null) {
-                    a = targetArray[j].getHitBox();
+        	for (j = 0; j < targetArray.length; j++) {
+                
+        		if (targetArray[j] != null) {
+        			random = Math.random();
+                    if (random > 0.1 && random < .11) {
+                    	xt[j] = 0; yt[j] = 1;
+                    }
+                    if (random > 0.2 && random < .21) {
+                    	xt[j] = 0; yt[j] = -1;
+                    }
+                    if (random > 0.3 && random < .31) {
+                    	xt[j] = -1; yt[j] = 0;
+                    }
+                    if (random > 0.4 && random < .41) {
+                    	xt[j] = 1; yt[j] = 0;
+                    }
+        			targetArray[j].move(xt[j], yt[j]);
+                	a = targetArray[j].getHitBox();
                     Zen.fillRect(targetArray[j].getX(), targetArray[j].getY(), a[1] - a[0] - 10, a[3] - a[2] - 10);
                 } else {
                     a = null;
@@ -70,7 +91,7 @@ public class SquareSploders {
                         if (targetArray[j] != null) {
                             a = targetArray[j].getHitBox();
                             if (laser.getX() >= a[0] && laser.getX() <= a[1] && laser.getY() >= a[2] && laser.getY() <= a[3]) {
-                                targetArray[j] = null; a = null;
+                                targetArray[j] = null; a = null; laserray[i] = null;
                             }
                         } else {
                             a = null;
@@ -82,6 +103,7 @@ public class SquareSploders {
             for (j = 0; j < targetArray.length; j++) {
                 if (targetArray[j] != null) {
                     s1 = true;
+                    
                 }
             }
             if (s1 == false) {
