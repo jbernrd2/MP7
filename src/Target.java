8875eca86760cf952dd0702b420de33ed7cc6443
Jack Bernard <jbernrd2@illinois.edu>
2017-12-12 22:51:31 -0600

    Idk what i did tbh

--- src/Target.java
+++ src/Target.java
@@ -2,13 +2,13 @@ import java.util.Random;
 
 public class Target {
     private int x; private int y; private int hitXmin; private int hitYmin;
-    private int hitXmax; private int hitYmax;
+    private int hitXmax; private int hitYmax; private int l, h;
 
     public Target() {
     }
     public Target(int i,int j, int length, int height) {
-        this.x = i; this.y = j; this.hitXmin = i - length/2; this.hitYmin = j - length/2;
-        this.hitXmax = i + length/2; this.hitYmax = j + length/2;
+        this.x = i; this.y = j; this.hitXmin = i - length/2; this.hitYmin = j - height/2;
+        this.hitXmax = i + length/2; this.hitYmax = j + height/2; this.l = length; this.h = height;
     }
     public int[] getHitBox() {
 
@@ -29,9 +29,11 @@ public class Target {
     public void move(int i, int j) {
         if (this.x + i > 100 && this.x < 600) {
             this.x += i;
+            this.hitXmin = this.x - l/2; this.hitXmax = this.x + l/2;
         }
         if (this.y + j > 100 && this.y + j < 900) {
             this.y += j;
+            this.hitYmin = this.y - h/2; this.hitYmax = this.y + h/2;
         }
     }
 }
